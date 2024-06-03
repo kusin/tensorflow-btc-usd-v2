4ac3e94dc2a2f7e94b9ba2cb53fd3a5e0dfa7ac5
kusin <aryajayaalamsyah@gmail.com>
2024-06-03 08:40:49 +0700

    -

--- JupyterLab/C02_visualization.py
+++ JupyterLab/C02_visualization.py
@@ -7,8 +7,8 @@ def timeseries_matplotlib(df, nm_labels):
 
     # create lineplot
     fig, ax = plt.subplots(figsize = (8,4))
-    for x in range(len(df.columns.values)):
-      ax.plot(df["Date"], df.iloc[:, x:x+1], label=nm_labels[x], linewidth=2.5)
+    for x in range(len(nm_labels)):
+      ax.plot(df.iloc[:, 0:1], df.iloc[:, x+1:x+2], label=nm_labels[x], linewidth=2.5)
 
     # set label-labels
     ax.set_title("", fontsize=12)
@@ -38,12 +38,12 @@ def lineplot_matplotlib1(df, nm_labels):
     return plt.show()
 # ----------------------------------------------------------------------------------------
 
-def lineplot_matplotlib2(df, nm_labels):
+def lineplot_matplotlib2(line1, label1, line2, label2):
 
     # create lineplot
     fig, ax = plt.subplots(figsize = (8,4))
-    ax.plot(df, label=nm_labels, linewidth=2.5)
-    ax.plot(df, label=nm_labels, linewidth=2.5)
+    ax.plot(line1, color="tab:blue", label=label1, linewidth=2)
+    ax.plot(line2, color="tab:red", label=label2, linewidth=2)
     
     # set label-labels
     ax.set_title("", fontsize=12)
---
+++ JupyterLab/C05_model_evaluate.py
@@ -0,0 +1,6 @@
+# lib evaluate models
+from math import sqrt
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+# ----------------------------------------------------------------------------------------
