fdb40e4e6ae9e2a4922a175e0c3d1e4bf95d0baa
kusin <aryajayaalamsyah@gmail.com>
2024-06-04 06:48:29 +0700

    -

--- JupyterLab/C04_model_predictions.py
+++ JupyterLab/C04_model_predictions.py
@@ -32,7 +32,6 @@ def get_models(algorithm, x_train, y_train, x_test, y_test):
     tf.keras.backend.clear_session()
     model = tf.keras.Sequential([
       tf.keras.layers.Bidirectional(GRU(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1))),
-      tf.keras.layers.Dropout(0.05),
       tf.keras.layers.Bidirectional(GRU(units=50, return_sequences=False)),
       tf.keras.layers.Dropout(0.05),
       tf.keras.layers.Dense(1)
