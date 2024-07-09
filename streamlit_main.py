29ccf248f5463cadbe68c1c862ede4941b296a31
kusin <aryajayaalamsyah@gmail.com>
2024-07-10 05:11:33 +0700

    -

---
+++ streamlit_main.py
@@ -0,0 +1,72 @@
+# import library streamlit
+import streamlit as st
+
+# lib visualization data
+import plotly.express as px
+import plotly.graph_objects as go
+
+# import custom func
+from streamlit_visualization import *
+
+# config web streamlit
+st.set_page_config(
+  page_title="My Dasboard - Cryptocurrency", 
+  page_icon="🧊",
+  layout="wide",
+  initial_sidebar_state="auto",
+  menu_items={
+    "Get Help": "https://www.github.com/kusin",
+    "Report a bug": "https://www.github.com/kusin",
+    "About": "#### Copyright 2024, By Aryajaya Alamsyah"
+  }
+)
+
+# load-dataset
+dataset = getData()
+
+# container-header
+with st.container():
+  st.markdown("# Model Prediction of BTC-USD Price Using Neural Network")
+
+# container-content
+with st.container():
+  
+  # split two-columns
+  col1, col2 = st.columns([0.4,0.6], gap="small")
+  
+  # show-dataset
+  col1.success("Dataset of BTC-USD Price")
+  col1.dataframe(dataset.sort_values(by=["Date"], ascending=False), use_container_width=True)
+  
+  # show-visualization
+  col2.success("Data Visualization of BTC-USD Price")
+  col2.plotly_chart(timeseries_plot(dataset))
+
+# container-content
+with st.container():
+
+  # split three-columns
+  col1, col2, col3 = st.columns([0.2,0.2,0.6], gap="small")
+
+  # col1-config-model
+  with col1:
+    st.success("Config Model")
+    with st.form("my-form"):
+      optimizers = st.selectbox(label="Choose a optimizers", options=("adam", "adamax", "rmsprop", "sgd"), placeholder="Choose a optimizers", index=None)
+      batch_size = st.selectbox(label="Choose a batch size", options=("4", "8", "16", "32", "64"), placeholder="Choose a optimizers", index=None)
+      epoch = st.selectbox(label="Choose a optimizers", options=("25", "50", "75", "100"), placeholder="Choose a optimizers", index=None)
+      agree = st.checkbox("Using the default settings")
+      submit = st.form_submit_button(label="Submit", type="secondary", use_container_width=True)
+  
+  # col2-results-prediction
+  with col2:
+    st.success("Results Prediction")
+    st.text("R    : 0.999")
+    st.text("MAE  : 0.999")
+    st.text("RMSE : 0.999")
+    st.text("MAPE : 0.999")
+
+  # col2-data-visualization
+  with col3:
+    st.success("Data Visualization of BTC-USD Price")
+    st.plotly_chart(timeseries_plot(dataset))
---
+++ streamlit_visualization.py
@@ -0,0 +1,44 @@
+# lib manipulation dat
+import pandas as pd
+import numpy as np
+
+# lib visualization data
+import plotly.express as px
+import plotly.graph_objects as go
+# ---------------------------------------------------------------------------
+
+# func load-datase
+def getData():
+  dataset = pd.read_csv("dataset/BTC-USD.csv")
+  dataset = dataset[["Date", "Open", "High", "Low", "Close"]]
+  return np.round(dataset,2)
+
+# func build heatmap coor
+def timeseries_plot(df):
+
+  # create a plot
+  fig = go.Figure()
+  
+  # add lineplot with graph object
+  for column in df.columns[1:]:
+    fig.add_trace(
+      go.Scatter(
+          x=df["Date"],y=df[column], mode='lines', name=column
+      )
+    )
+  
+  # add colors on lineplot
+  colorscale = px.colors.diverging.Portland_r
+  for i, trace in enumerate(fig.data):
+    trace.update(line=dict(color=colorscale[i]))
+
+  # update layout lineplot
+  fig.update_layout(
+    title = "Visualization Timeseries of BTC-USD Price",
+    xaxis_title = "",
+    yaxis_title = "",
+    legend=dict(title='', orientation='h', yanchor='top', y=1.1, xanchor='center', x=0.5),
+  )
+
+  # return values
+  return fig
