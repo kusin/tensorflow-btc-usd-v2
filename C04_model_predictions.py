00ef2e90013ed5adf2fd07f5af9eb18d2e39f5af
kusin <aryajayaalamsyah@gmail.com>
2024-08-14 22:15:05 +0700

    -

--- C04_model_predictions.py
+++ C04_model_predictions.py
@@ -9,8 +9,8 @@ from keras.callbacks import EarlyStopping
 def get_models(algorithm, x_train, y_train, x_test, y_test):
 
   # set parameter tuning
-  optimizers = "adamax"
-  batch_size = 16
+  optimizers = "rmsprop"
+  batch_size = 8
   epoch = 50
 
   # 1. The LSTM architecture
